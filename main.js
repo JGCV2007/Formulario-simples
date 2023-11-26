e14b3e084b1856581fba82ebd116b3c2ebe24401
JGCV2007 <jeangcv@hotmail.com>
2023-11-26 06:25:26 -0300

    Atualizado

---
+++ main.js
@@ -0,0 +1,10 @@
+var inpunt = document.getElementById('Iemail');
+var output = document.getElementById('output');
+var error = document.getElementById('errror.message');
+function validar(){
+    if(inpunt.value === " "){
+        error.innerText = 'Valid email required';
+    }
+    
+    else(window.location= "file:///C:/Users/Jean/Documents/curso%20HTML%20e%20CSS/HTML5%20e%20CSS3/Font-end/Atvidades-do-front-end/Formul%C3%A1rio/Formulario-simples/success.html")
+}
